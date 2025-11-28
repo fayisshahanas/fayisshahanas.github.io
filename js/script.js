ec7dc9b72087e52b9c9182fe903701d2f0a1be75
fayisshahanas <shahanassalam@gmail.com>
2025-11-28 23:08:19 +0530

    yes

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+//     $('.toggle-bt').click(function(){
+//         $('.toggle-bt').toggleClass('toggled');
+//         $('header nav ul').toggleClass('toggled');
+//     });
+// })
+$(document).ready(function(){
+
+
+
+  $(".toggle_btn").click(function(){
+    $(this).toggleClass("toggle_active")
+     $(".sidebar").toggleClass("sidebar_active");
+  });
+
+  
+  
+});
